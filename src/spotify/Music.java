24fa74e5c979265831a5d936de0de31e676938b8
fati <fatemesadatsedigh@gmail.com>
2025-04-18 16:08:44 +0330

    add fields

--- src/spotify/Music.java
+++ src/spotify/Music.java
@@ -1,4 +1,14 @@
 package spotify;
 
+import java.util.ArrayList;
+
 public class Music {
+
+    private String title;
+    private User singer;
+    private int numberOfStream;
+    private static ArrayList<Music> allMusics = new ArrayList<>();
+
+
+
 }
