db676028b397d354ccce46f6481401fbad78995e
fati <fatemesadatsedigh@gmail.com>
2025-04-18 12:33:21 +0330

    add UserBehavior fields

--- src/spotify/UserBehavior.java
+++ src/spotify/UserBehavior.java
@@ -1,4 +1,9 @@
 package spotify;
 
 public interface UserBehavior {
+
+    void createPlaylist (String Title, User Owner);
+    void playMusic (Music music);
+    void buyPremium (User owner, int month);
+
 }
