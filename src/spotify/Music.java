77e24f09dd568cb58da69ad59e5dfdf9e5405244
fati <fatemesadatsedigh@gmail.com>
2025-04-18 16:13:39 +0330

    add public Music

--- src/spotify/Music.java
+++ src/spotify/Music.java
@@ -9,6 +9,10 @@ public class Music {
     private int numberOfStream;
     private static ArrayList<Music> allMusics = new ArrayList<>();
 
-
+    public Music(String title, User singer){
+        this.title = title;
+        this.singer = singer;
+        allMusics.add(this);
+    }
 
 }
