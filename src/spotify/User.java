2c99ee1f9e504d3f5a16782d24c0c8ee7d899e6a
fati <fatemesadatsedigh@gmail.com>
2025-04-18 14:45:37 +0330

    add User fields

--- src/spotify/User.java
+++ src/spotify/User.java
@@ -1,4 +1,17 @@
 package spotify;
 
+import java.util.ArrayList;
+
 public class User {
+
+    private String username;
+    private String password;
+    private ArrayList<User> followerList = new ArrayList<>();
+    private ArrayList<User> followingList = new ArrayList<>();
+    private UserBehavior behavior;
+    private ArrayList<Playlist> playlists = new ArrayList<>();
+    private static ArrayList<User> allUsers;
+
+
+
 }
