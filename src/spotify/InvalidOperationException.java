dd103f7d31e49119cc8f4a7fe30447b026ab0f21
fati <fatemesadatsedigh@gmail.com>
2025-04-18 14:32:35 +0330

    add InvalidOperationException

--- src/spotify/InvalidOperationException.java
+++ src/spotify/InvalidOperationException.java
@@ -1,4 +1,9 @@
 package spotify;
 
-public class InvalidOperationException {
+public class InvalidOperationException extends RuntimeException {
+
+    public InvalidOperationException (String message){
+        super(message);
+    }
+
 }
