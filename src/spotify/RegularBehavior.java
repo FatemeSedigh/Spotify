7da60890043887cd4262e921ccf74d63e203e6ce
fati <fatemesadatsedigh@gmail.com>
2025-04-18 12:55:20 +0330

    add @Override

--- src/spotify/RegularBehavior.java
+++ src/spotify/RegularBehavior.java
@@ -2,4 +2,23 @@ package spotify;
 
 public class RegularBehavior implements UserBehavior {
 
+
+
+    @Override
+    void createPlaylist (String Title, User Owner){
+
+    }
+
+    @Override
+    void playMusic (Music music){
+
+    }
+
+    @Override
+    void buyPremium (User owner, int month){
+
+    }
+
+
+
 }
