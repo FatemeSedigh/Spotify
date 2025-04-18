ff081a50292d7595085eda336cd38b413c5bfdcf
fati <fatemesadatsedigh@gmail.com>
2025-04-18 13:47:50 +0330

    add Override

--- src/spotify/PremiumBehavior.java
+++ src/spotify/PremiumBehavior.java
@@ -2,4 +2,20 @@ package spotify;
 
 public class PremiumBehavior implements UserBehavior {
 
+    @Override
+    void createPlaylist (String Title, User Owner){
+
+    }
+
+    @Override
+    void playMusic (Music music){
+
+    }
+
+    @Override
+    void buyPremium (User owner, int month){
+
+    }
+
+
 }
