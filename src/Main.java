330b48a9702e982e6000618951797330d8bf50aa
fati <fatemesadatsedigh@gmail.com>
2025-04-18 23:52:13 +0330

    add test main

--- src/Main.java
+++ src/Main.java
@@ -11,24 +11,19 @@ public class Main {
             user2.follow(user1);
             user1.follow(artist);
 
-            // Create music
             Music song1 = new Music("Song1", artist);
             Music song2 = new Music("Song2", artist);
 
-            // User1 plays music
             user1.playMusic(song1);
             user1.playMusic(song2);
 
-            // User1 buys premium
             user1.buyPremium(1);
 
-            // Create playlist
             user1.createPlaylist("My Playlist");
             Playlist playlist = user1.getPlaylists().get(0);
             playlist.addMusic(song1, "password123");
             playlist.addMusic(song2, "password123");
 
-            // Play playlist
             playlist.playPlaylist();
 
         } catch (InvalidOperationException e) {
